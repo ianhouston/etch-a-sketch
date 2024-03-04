cc8320d78e7cb0fbdbd9d6a5d99eefc384b4634a
ianhouston <smooveslushie@gmail.com>
2024-03-03 19:47:54 -0600

    improved grid, but still needs work for keeping grid relative to window

--- javascript.js
+++ javascript.js
@@ -2,23 +2,30 @@ const gridChangeButton = document.querySelector('button');
 
 let grid2dArray = [];
 
+const BASE_SIZE = 4;
+const titleBannerHeight = 80;
 const gridContainerSpacePx = 8;
 const gridContainer = document.createElement('div');
-gridContainer.setAttribute('style', `background-color: lightgray; display: flex; gap: ${gridContainerSpacePx}px; margin: ${gridContainerSpacePx}px; flex-wrap: wrap;`)
+gridContainer.setAttribute('style', `background-color: lightgray; display: flex; flex-wrap: wrap; justify-content: center;
+                            gap: ${gridContainerSpacePx}px; margin: ${gridContainerSpacePx}px;
+                            max-width: ${(((window.innerHeight/BASE_SIZE)-(gridContainerSpacePx*(BASE_SIZE+1))-(titleBannerHeight/BASE_SIZE))*BASE_SIZE)+(gridContainerSpacePx*(BASE_SIZE+1))}px;`)
 document.querySelector('body').appendChild(gridContainer);
 
 //creates a square grid based on the constant BASE_SIZE
 // the grid would be BASE_SIZE * BASE_SIZE
-const BASE_SIZE = 4;
 for (let i = 0; i < BASE_SIZE; i++) {
     grid2dArray.push([]);
     for (let j = 0; j < BASE_SIZE; j++) {
         grid2dArray[i].push(document.createElement('div'));
-        grid2dArray[i][j].setAttribute('style', `background-color: darkgray; width: ${(window.innerWidth/BASE_SIZE)-(gridContainerSpacePx*BASE_SIZE)}px; height: ${(window.innerHeight/BASE_SIZE)-(gridContainerSpacePx*BASE_SIZE)}px; flex-basis: 24%`);
+        grid2dArray[i][j].setAttribute('style', `background-color: darkgray; 
+                                        width: ${(window.innerHeight/BASE_SIZE)-(gridContainerSpacePx*(BASE_SIZE+1))-(titleBannerHeight/BASE_SIZE)}px; 
+                                        height: ${(window.innerHeight/BASE_SIZE)-(gridContainerSpacePx*(BASE_SIZE+1))-(titleBannerHeight/BASE_SIZE)}px;
+                                        flex-grow: 0;`);
         gridContainer.appendChild(grid2dArray[i][j]);
     }
 }
 
+//clears grid array and grid container
 const clearGrid = () => {
     grid2dArray = [];
     while (gridContainer.lastElementChild) {
@@ -26,20 +33,24 @@ const clearGrid = () => {
     }
 }
 
+//makes a new grid according to the given integer size
 const changeGrid = (newSize) => {
     clearGrid();
+    gridContainer.style.maxWidth = `${(((window.innerHeight/newSize)-(gridContainerSpacePx*(newSize+1))-(titleBannerHeight/newSize))*newSize)+(gridContainerSpacePx*(newSize+1))}px`;
     for (let i = 0; i < newSize; i++) {
         grid2dArray.push([]);
         for (let j = 0; j < newSize; j++) {
             grid2dArray[i].push(document.createElement('div'));
-            grid2dArray[i][j].setAttribute('style', `background-color: darkgray; width: ${(window.innerWidth/newSize)-(gridContainerSpacePx*newSize)}px; height: ${(window.innerHeight/newSize)-(gridContainerSpacePx*newSize)}px; flex-basis: 24%`);
+            grid2dArray[i][j].setAttribute('style', `background-color: darkgray; 
+                                            width: ${(window.innerHeight/newSize)-(gridContainerSpacePx*(newSize+1))-(titleBannerHeight/newSize)}px; 
+                                            height: ${(window.innerHeight/newSize)-(gridContainerSpacePx*(newSize+1))-(titleBannerHeight/newSize)}px; 
+                                            flex-grow: 0;`);
             gridContainer.appendChild(grid2dArray[i][j]);
         }
     }
 }
 
 // Adding an event for the grid change button
-
 const promptNewGridSize = () => {
     changeGrid(parseInt(prompt("What length do you want your new square grid to have?")));
 }
