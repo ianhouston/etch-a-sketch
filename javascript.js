6b6771de20792ecd3ac13e0ddb2f335abbc55e32
ianhouston <smooveslushie@gmail.com>
2024-03-04 19:59:23 -0600

    fixed grid getting smaller with greater number of grid members and placed a limit of 100 on grid length

--- javascript.js
+++ javascript.js
@@ -2,13 +2,18 @@ const gridChangeButton = document.querySelector('button');
 
 let grid2dArray = [];
 
-const BASE_SIZE = 4;
+const BASE_SIZE = 16;
 const titleBannerHeight = 80;
-const gridContainerSpacePx = 8;
+const gridMarginPx = 2;
 const gridContainer = document.createElement('div');
-gridContainer.setAttribute('style', `background-color: lightgray; display: flex; flex-wrap: wrap; justify-content: center;
-                            gap: ${gridContainerSpacePx}px; margin: ${gridContainerSpacePx}px;
-                            max-width: ${(((window.innerHeight/BASE_SIZE)-(gridContainerSpacePx*(BASE_SIZE+1))-(titleBannerHeight/BASE_SIZE))*BASE_SIZE)+(gridContainerSpacePx*(BASE_SIZE+1))}px;`)
+gridContainer.setAttribute('style', `
+                            background-color: lightgray; 
+                            display: flex; 
+                            flex-wrap: wrap; 
+                            justify-content: center;
+                            margin: ${gridMarginPx}px;
+                            margin-bottom: 0;
+                            max-width: ${(((window.innerHeight/BASE_SIZE)-(titleBannerHeight/BASE_SIZE))*BASE_SIZE)}px;`)
 document.querySelector('body').appendChild(gridContainer);
 
 //creates a square grid based on the constant BASE_SIZE
@@ -18,9 +23,8 @@ for (let i = 0; i < BASE_SIZE; i++) {
     for (let j = 0; j < BASE_SIZE; j++) {
         grid2dArray[i].push(document.createElement('div'));
         grid2dArray[i][j].setAttribute('style', `background-color: darkgray; 
-                                        width: ${(window.innerHeight/BASE_SIZE)-(gridContainerSpacePx*(BASE_SIZE+1))-(titleBannerHeight/BASE_SIZE)}px; 
-                                        height: ${(window.innerHeight/BASE_SIZE)-(gridContainerSpacePx*(BASE_SIZE+1))-(titleBannerHeight/BASE_SIZE)}px;
-                                        flex-grow: 0;`);
+                                        width: ${(window.innerHeight/BASE_SIZE)-(titleBannerHeight/BASE_SIZE)}px; 
+                                        height: ${(window.innerHeight/BASE_SIZE)-(titleBannerHeight/BASE_SIZE)}px;`);
         gridContainer.appendChild(grid2dArray[i][j]);
     }
 }
@@ -50,17 +54,20 @@ function changeGrid(newSize) {
         alert("That is not a valid length. Please enter an integer when the next prompt appears.");
         promptNewGridSize();
     }
+    else if (newSize > 100) {
+        alert("100 is the maximum length for the grid. Please enter a smaller number when the next prompt appears.")
+        promptNewGridSize();
+    }
     else {
         clearGrid();
-        gridContainer.style.maxWidth = `${(((window.innerHeight/newSize)-(gridContainerSpacePx*(newSize+1))-(titleBannerHeight/newSize))*newSize)+(gridContainerSpacePx*(newSize+1))}px`;
+        gridContainer.style.maxWidth = `${(((window.innerHeight/newSize)-(titleBannerHeight/newSize))*newSize)}px`;
         for (let i = 0; i < newSize; i++) {
             grid2dArray.push([]);
             for (let j = 0; j < newSize; j++) {
                 grid2dArray[i].push(document.createElement('div'));
                 grid2dArray[i][j].setAttribute('style', `background-color: darkgray; 
-                                                width: ${(window.innerHeight/newSize)-(gridContainerSpacePx*(newSize+1))-(titleBannerHeight/newSize)}px; 
-                                                height: ${(window.innerHeight/newSize)-(gridContainerSpacePx*(newSize+1))-(titleBannerHeight/newSize)}px; 
-                                                flex-grow: 0;`);
+                                                width: ${(window.innerHeight/newSize)-(titleBannerHeight/newSize)}px; 
+                                                height: ${(window.innerHeight/newSize)-(titleBannerHeight/newSize)}px; `);
                 gridContainer.appendChild(grid2dArray[i][j]);
             }
         }
